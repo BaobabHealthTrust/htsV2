398bc9f0189bccb522fb56dbfe5cd18fe81b67af
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2018-06-06 09:49:19 +0200

    Added logic for dynamically settings database configurations

---
+++ server/datasources._env_.js
@@ -0,0 +1,8 @@
+const settings = require('../configs/database.json');
+module.exports = {
+    host: process.env.DB_HOST || settings[process.env.NODE_ENV || 'development'].host,
+    user: process.env.DB_USER || settings[process.env.NODE_ENV || 'development'].user,
+    password: process.env.DB_PASSWORD || settings[process.env.NODE_ENV || 'development'].password,
+    database: process.env.DB_DATABASE || settings[process.env.NODE_ENV || 'development'].database,
+    port: process.env.DB_PORT || settings[process.env.NODE_ENV || 'development'].port
+}
