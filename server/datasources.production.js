d2868874a79d29136a0a5270381e5b3a8d8c8270
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2018-08-17 14:49:31 +0200

    Updated logic for Loopback datasources

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,11 +1,20 @@
 const settings = require('../configs/database.json');
 
+const env = 'production';
+const host = process.env.DB_HOST || settings[process.env.NODE_ENV || env].host;
+const user = process.env.DB_USER || settings[process.env.NODE_ENV || env].user;
+const password = process.env.DB_PASSWORD || settings[process.env.NODE_ENV || env].password;
+const database = process.env.DB_DATABASE || settings[process.env.NODE_ENV || env].database;
+
 module.exports = {
     hts: {
-        host: process.env.DB_HOST || settings[process.env.NODE_ENV || 'production'].host,
-        user: process.env.DB_USER || settings[process.env.NODE_ENV || 'production'].user,
-        password: process.env.DB_PASSWORD || settings[process.env.NODE_ENV || 'production'].password,
-        database: process.env.DB_DATABASE || settings[process.env.NODE_ENV || 'production'].database,
-        port: process.env.DB_PORT || settings[process.env.NODE_ENV || 'production'].port
+        host,
+        user,
+        password,
+        database,
+        port: process.env.DB_PORT || settings[process.env.NODE_ENV || env].port,
+        url: `mysql://${user}:${password}@${host}/${database}`,
+        name: "hts",
+        connector: "mysql"
     }
 }
