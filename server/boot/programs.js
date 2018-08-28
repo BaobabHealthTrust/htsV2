8eff3461d165c8cf8d8b99957ebee753b3313dad
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2018-08-28 16:24:58 +0200

    Added logic to save encounter as confirmatory testing type if so

--- server/boot/programs.js
+++ server/boot/programs.js
@@ -1690,7 +1690,11 @@ module.exports = function (app) {
       ? (new Date(person.birthdate)).format("YYYY-mm-dd")
       : null;
 
-    let encounterName = "HTS Visit";
+    let encounterName = (Object.keys(json['HTS Visit']).indexOf("Result Given to Client") >= 0 && ["Confirmatory Positive", "Confirmatory (Antibody) Positive"].indexOf(json['HTS Visit']["Result Given to Client"]) >= 0 ? "Confirmatory HIV Testing" : "HTS Visit");
+
+    debug(encounterName);
+
+    json[encounterName] = json["HTS Visit"];
 
     encType = await EncounterType.findOne({
       where: {
@@ -2123,7 +2127,7 @@ module.exports = function (app) {
 
     debug("*****************");
 
-    debug(json);
+    debug(JSON.stringify(json));
 
     debug("*****************");
 
@@ -2321,8 +2325,12 @@ module.exports = function (app) {
 
     let patientProgramId = patientProgram.patientProgramId;
 
+    let encounterName = (Object.keys(json).indexOf("Result Given to Client") >= 0 && ["Confirmatory Positive", "Confirmatory (Antibody) Positive"].indexOf(json["Result Given to Client"]) >= 0 ? "Confirmatory HIV Testing" : "HTS Visit");
+
+    debug(encounterName);
+
     let groups = {
-      "HTS Visit": [
+      [encounterName]: [
         "Age Group",
         "HTS Access Type",
         "Last HIV Test",
@@ -2345,13 +2353,13 @@ module.exports = function (app) {
       ]
     };
 
-    let encounterName = "HTS Visit";
-
     let encType = await EncounterType.findOne({
       where: {
         name: encounterName
       }
     });
+
+    debug(encType);
 
     let encounterType = encType
       ? encType.encounterTypeId
