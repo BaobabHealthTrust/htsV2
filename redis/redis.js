0d3e413d30c96b330d382c7ffbdd465e309fbb9c
danielmwakanema <danielmwakanema95@gmail.com>
2019-07-05 10:16:42 +0200

    refactor(Redis): Minor tweaks to Redis script

--- redis/redis.js
+++ redis/redis.js
@@ -1,18 +1,8 @@
 const RedisServer = require('redis-server');
 
-// Simply pass the port that you want a Redis server to listen on.
 const server = new RedisServer(6379);
 
-server.open((err) => {
-  if (err === null) {
-    // You may now connect a client to the Redis server bound to `server.port` (e.g.
-    // 6379).
-
-    console.log("Redis running on port 6379");
-
-  } else {
-
-    console.log(err);
-
-  }
+server.open(error => {
+  if (error) { console.log(`Unable to start Redis: ${ error.message }`); process.exit(1) }
+  else console.log('Redis is running on port 6379')
 });
